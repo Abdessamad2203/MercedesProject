5ad3bf608efa953716730cb5ff5c8416d9b2d5a9
Moumad Abdessamad (s121066) <abdessamad.moumad@student.ap.be>
2023-01-04 18:15:05 +0100

    git commit .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,93 @@
 import * as React from "react"
 import Layout from "../components/Layout"
 import {mainindex,indextitle , picturemain, button2, button} from "../pages/Style/index.css"
+import { Link , graphql} from "gatsby"
+
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import MercedesPage from "./mercedes/{wpMercedes.slug}"
+
+
   
 
-const IndexPage = () => {
+
+const IndexPage = ({ data: { wpPage: { homeFields } },}) => {
+  const image = getImage(homeFields.picture.localFile)
+
   return (
-    <>
     <Layout>
       <div className="mainindex">
       <p>------------------------------------------------------------------------------------------------------------------------------</p>
-        <h1 className="indextitle">Welcome to my Mercedes Salon</h1>
-        <img className="picturemain" src="https://www.pngmart.com/files/2/Mercedes-Logo-PNG-Image.png" alt="Mercedes Image" /> <br></br>
+        <h1 className="indextitle">{homeFields.title}</h1>
+        <GatsbyImage image = {image}></GatsbyImage>
+        
+        {
+          homeFields.feauturedProducts.map((item) => {
+            const mercedes = item.mercedes
+            const slug = item.slug
+            const image = getImage(mercedes.picture.localFile)
+
+            return <div> 
+            <Link to = {`/mercedes/${slug}`} >
+              <GatsbyImage
+                image={image}
+                
+                
+              />
+              <p key={item.id}  style={{fontSize:"23px"}}><b>{mercedes.title}</b></p>
+              <p >{mercedes.description}</p>
+              </Link>
+            </div>
+          })
+        }
+
+        
+        
+        
+        <br></br>
         <a href="/mercedes">        
         <button class="button button2" >Click here to check out my Favorite Mercedes Cars!</button>
         </a>
         <p>------------------------------------------------------------------------------------------------------------------------------</p>
       </div>
       </Layout>
-      </>
   )
 }
+
+
+export const query = graphql`
+
+query  {
+  wpPage(slug: {eq: "home"}) {
+    homeFields {
+      description
+      title
+      picture {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+        }
+      }
+      feauturedProducts {
+        ... on WpMercedes {
+          id
+          slug
+          mercedes {
+            title
+            description
+            picture {
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(placeholder: BLURRED)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
